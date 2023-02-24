1b9b2ad41c85ade487d3d0145ee76240313f34b6
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-02-24 18:13:22 +0100

    test: 💍 Added tests for `time` utitlity

---
+++ test/utils.time.spec.ts
@@ -0,0 +1,35 @@
+import 'mocha';
+import { expect } from 'chai';
+import { validateDurationFormat, parseSeconds } from '../src/utils/time.js';
+
+describe('Utils.time', () => {
+  describe('#validateDurationFormat', () => {
+    it('should throw if format unknown', async () => {
+      expect(() => validateDurationFormat('t')).to.throw('Unknown duration time format');
+    });
+
+    it('should validate known types', async () => {
+      expect(() => validateDurationFormat('ms')).not.to.throw;
+      expect(() => validateDurationFormat('s')).not.to.throw;
+      expect(() => validateDurationFormat('m')).not.to.throw;
+      expect(() => validateDurationFormat('h')).not.to.throw;
+      expect(() => validateDurationFormat('d')).not.to.throw;
+      expect(() => validateDurationFormat('y')).not.to.throw;
+    });
+  });
+
+  describe('#parseSeconds', () => {
+    it('should throw if format unknown', async () => {
+      expect(() => parseSeconds('t5')).to.throw('Unknown duration time format');
+    });
+
+    it('should validate known types', async () => {
+      expect(parseSeconds('1ms')).to.eq(0);
+      expect(parseSeconds('1s')).to.eq(1);
+      expect(parseSeconds('1m')).to.eq(60);
+      expect(parseSeconds('1h')).to.eq(60 * 60);
+      expect(parseSeconds('1d')).to.eq(60 * 60 * 24);
+      expect(parseSeconds('1y')).to.eq(60 * 60 * 24 * 365.25);
+    });
+  });
+});
