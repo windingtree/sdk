69f4f91025527c18c4dbd37df61081581ed759ab
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-02-24 17:16:41 +0100

    feat: 🎸 Added uuid4() utility

--- src/utils/uid.ts
+++ src/utils/uid.ts
@@ -11,3 +11,11 @@ export const simpleUid = (length = 14): string => {
     .join('')
     .slice(0, length);
 };
+
+// Generates UUID v4
+export const uuid4 = (): string =>
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(36);
+  });
