06d69a71ca6e8ca3bd070fa322bc02be649a9e50
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-02-24 17:18:36 +0100

    feat: 🎸 Added parseSeconds(str) utility
    1m, 2d, 1y => seconds

---
+++ src/utils/time.ts
@@ -0,0 +1,64 @@
+// Calculation bases
+const s = 1;
+const ms = s * 0.001;
+const m = s * 60;
+const h = m * 60;
+const d = h * 24;
+const y = d * 365.25;
+
+export type AllowedDurationType = 'ms' | 's' | 'm' | 'h' | 'd' | 'y';
+
+export interface DurationFormat {
+  type: AllowedDurationType;
+  value: number;
+}
+
+export const validateDurationFormat = (str: string): DurationFormat => {
+  const match = /^((?:\d+)?\.?\d+) *(ms|s|m|h|d|y)?$/i.exec(str); // milliseconds|seconds|minutes|hours|days|years
+
+  if (!match) {
+    throw new Error('Unknown duration time format');
+  }
+
+  return {
+    value: parseFloat(match[1]),
+    type: (match[2] || 's').toLocaleLowerCase() as AllowedDurationType,
+  };
+};
+
+// Parses formatted string into seconds number value
+export const parseSeconds = (str: string | number): number => {
+  if (typeof str === 'number') {
+    return str;
+  }
+
+  str = String(str);
+
+  const { type, value } = validateDurationFormat(str);
+  let parsed = 0;
+
+  switch (type) {
+    case 'ms':
+      parsed = value * ms;
+      break;
+    case 's':
+      parsed = value * s;
+      break;
+    case 'm':
+      parsed = value * m;
+      break;
+    case 'h':
+      parsed = value * h;
+      break;
+    case 'd':
+      parsed = value * d;
+      break;
+    case 'y':
+      parsed = value * y;
+      break;
+    default:
+      throw new Error('Unknown duration type');
+  }
+
+  return Math.ceil(parsed);
+};
