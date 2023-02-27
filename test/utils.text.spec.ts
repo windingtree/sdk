3bfa139e67f7decfa8be5eb54f7d8a14037b4eba
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-02-27 20:56:08 +0100

    test: 💍 Added tests for text utilities

---
+++ test/utils.text.spec.ts
@@ -0,0 +1,23 @@
+import 'mocha';
+import { expect } from 'chai';
+import { decodeText, encodeText } from '../src/utils/text.js';
+
+describe('Utils.text', () => {
+  describe('#decodeText', () => {
+    const text = 'test';
+
+    it('should decode text', async () => {
+      const encoded = new TextEncoder().encode(text);
+      expect(decodeText(encoded)).to.eq(text);
+    });
+  });
+
+  describe('#encodeText', () => {
+    const text = 'test';
+    const encoded = new TextEncoder().encode(text);
+
+    it('should encode text', async () => {
+      expect(encodeText(text)).to.deep.eq(encoded);
+    });
+  });
+});
