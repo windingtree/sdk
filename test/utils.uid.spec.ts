d5e32e53e973739126994d0a4e0ab043f54b7c06
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-02-28 11:28:53 +0100

    test: 💍 Added test for uuid4 utility

--- test/utils.uid.spec.ts
+++ test/utils.uid.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha';
 import { expect } from 'chai';
-import { simpleUid } from '../src/utils/uid.js';
+import { simpleUid, uuid4 } from '../src/utils/uid.js';
 
 describe('Utils.uid', () => {
   describe('#simpleUid', () => {
@@ -16,6 +16,22 @@ describe('Utils.uid', () => {
         set.add(simpleUid());
       }
       expect(set.size).to.be.eq(num);
+    });
+  });
+
+  describe('#uuid4', () => {
+    const match = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+
+    it('should generate uuid v4', async () => {
+      const set = new Set<string>();
+      const num = 10;
+      for (let i = 0; i < num; i++) {
+        set.add(uuid4());
+      }
+      expect(set.size).to.be.eq(num);
+      for (const id of set) {
+        expect(match.exec(id)).not.to.be.null;
+      }
     });
   });
 });
