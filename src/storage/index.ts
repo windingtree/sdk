1e127ed183a3656891d8bff7656f32eb034c35c0
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-03-10 10:21:53 +0100

    refactor: 💡 Updated exports from storage module

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,2 +1,5 @@
+import * as memoryStorage from './memory.js';
+import * as localStorage from './local.js';
+
 export * from './abstract.js';
-export * as memoryStorage from './memory.js';
+export { memoryStorage, localStorage };
