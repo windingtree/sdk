c5c01980d540924592f982e56da018abe64530cf
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-03-15 20:33:09 +0100

    feat: 🎸 Added noncePeriod constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const defaultExpirationTime = 30; // Default message expiration time in seconds
+export const defaultExpirationTime = '1m'; // Default message expiration time in seconds
 export const outboundStreamDelay = 250; // Time to wait for outbound stream in milliseconds
+export const noncePeriod = '10s'; // Time while the node accepting changes to request in seconds
