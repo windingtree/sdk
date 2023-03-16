bf81169291ae7f7d12777711af03a05b4794b2b8
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-03-16 21:29:16 +0100

    feat: 🎸 Added constants for Queue

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const defaultExpirationTime = '1m'; // Default message expiration time in seconds
 export const outboundStreamDelay = 250; // Time to wait for outbound stream in milliseconds
 export const noncePeriod = '10s'; // Time while the node accepting changes to request in seconds
+export const concurrentQueueJobs = 3; // Number of queue jobs that can be stated at once
+export const delayQueueJob = 2000; // Delay between repeats of failing jobs
