c07fb68d293ac911037be5b3faf0ad3d0c6ce8c8
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-03-28 01:48:06 +0200

    refactor: 💡 Offer and Request classes are disabled

--- src/shared/request.ts
+++ src/shared/request.ts
@@ -1,329 +1,306 @@
-import { EventEmitter, CustomEvent } from '@libp2p/interfaces/events';
-import { AbstractProvider } from 'ethers';
-import { z, ZodType } from 'zod';
-import {
-  GenericQuery,
-  RequestData,
-  createRequestDataSchema,
-  buildRequest,
-  GenericOfferOptions,
-} from './messages.js';
-import { Offer, RawOffer } from './offer.js';
-import { CenterSub } from './pubsub.js';
-import { encodeText } from '../utils/text.js';
-import { hashObject } from '../utils/hash.js';
-import { isExpired, nowSec } from '../utils/time.js';
-import { ContractConfig, ContractConfigSchema } from '../utils/contract.js';
-import { createLogger } from '../utils/logger.js';
+// import { EventEmitter, CustomEvent } from '@libp2p/interfaces/events';
+// import { AbstractProvider } from 'ethers';
+// import { z } from 'zod';
+// import {
+//   GenericQuery,
+//   RequestData,
+//   createRequestDataSchema,
+//   buildRequest,
+//   GenericOfferOptions,
+// } from './messages.js';
+// import { Offer, RawOffer } from './offer.js';
+// import { CenterSub } from './pubsub.js';
+// import { encodeText } from '../utils/text.js';
+// import { hashObject } from '../utils/hash.js';
+// import { isExpired, nowSec } from '../utils/time.js';
+// import { ContractConfig, ContractConfigSchema } from '../utils/contract.js';
+// import { createLogger } from '../utils/logger.js';
 
-const logger = createLogger('Request');
+// const logger = createLogger('Request');
 
-export const createRawRequestSchema = <CustomRequestQuery extends GenericQuery>(
-  querySchema: z.ZodType<CustomRequestQuery>,
-) =>
-  z
-    .object({
-      data: createRequestDataSchema<CustomRequestQuery>(querySchema),
-      topic: z.string(),
-      offers: z.array(z.string()),
-      published: z.number().optional(),
-      received: z.number().optional(),
-    })
-    .strict();
+// export const createRequestInitOptionsSchema = <
+//   CustomRequestQuery extends GenericQuery,
+//   CustomOfferOptions extends GenericOfferOptions,
+// >() =>
+//   z
+//     .object({
+//       querySchema: z.instanceof(ZodType<CustomRequestQuery>),
+//       offerOptionsSchema: z.instanceof(ZodType<CustomOfferOptions>),
+//       contractConfig: ContractConfigSchema,
+//       pubsub: z.instanceof(CenterSub),
+//       provider: z.instanceof(AbstractProvider).optional(),
+//     })
+//     .strict();
 
-// Request with metadata
-export interface RawRequest<
-  CustomRequestQuery extends GenericQuery,
-  CustomOfferOptions extends GenericOfferOptions,
-> {
-  data: RequestData<CustomRequestQuery>;
-  topic: string;
-  offers: RawOffer<CustomRequestQuery, CustomOfferOptions>[];
-  published?: number;
-  received?: number;
-}
+// export type RequestInitOptions<
+//   CustomRequestQuery extends GenericQuery,
+//   CustomOfferOptions extends GenericOfferOptions,
+// > = z.infer<
+//   ReturnType<typeof createRequestInitOptionsSchema<CustomRequestQuery, CustomOfferOptions>>
+// >;
 
-export const createRequestInitOptionsSchema = <
-  CustomRequestQuery extends GenericQuery,
-  CustomOfferOptions extends GenericOfferOptions,
->() =>
-  z
-    .object({
-      querySchema: z.instanceof(ZodType<CustomRequestQuery>),
-      offerOptionsSchema: z.instanceof(ZodType<CustomOfferOptions>),
-      contractConfig: ContractConfigSchema,
-      pubsub: z.instanceof(CenterSub),
-      provider: z.instanceof(AbstractProvider).optional(),
-    })
-    .strict();
+// export interface RequestEvents<
+//   CustomRequestQuery extends GenericQuery,
+//   CustomOfferOptions extends GenericOfferOptions,
+// > {
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('built', () => {
+//    *    // ... request built
+//    * })
+//    * ```
+//    */
+//   built: CustomEvent<void>;
 
-export type RequestInitOptions<
-  CustomRequestQuery extends GenericQuery,
-  CustomOfferOptions extends GenericOfferOptions,
-> = z.infer<
-  ReturnType<typeof createRequestInitOptionsSchema<CustomRequestQuery, CustomOfferOptions>>
->;
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('restored', () => {
+//    *    // ... request restored from raw data
+//    * })
+//    * ```
+//    */
+//   restored: CustomEvent<void>;
 
-export interface RequestEvents<
-  CustomRequestQuery extends GenericQuery,
-  CustomOfferOptions extends GenericOfferOptions,
-> {
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('built', () => {
-   *    // ... request built
-   * })
-   * ```
-   */
-  built: CustomEvent<void>;
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('published', () => {
+//    *    // ... published
+//    * })
+//    * ```
+//    */
+//   published: CustomEvent<void>;
 
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('restored', () => {
-   *    // ... request restored from raw data
-   * })
-   * ```
-   */
-  restored: CustomEvent<void>;
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('expired', () => {
+//    *    // ... expired
+//    * })
+//    * ```
+//    */
+//   expired: CustomEvent<void>;
 
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('published', () => {
-   *    // ... published
-   * })
-   * ```
-   */
-  published: CustomEvent<void>;
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('cancelled', () => {
+//    *    // ... cancelled
+//    * })
+//    * ```
+//    */
+//   cancelled: CustomEvent<void>;
 
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('expired', () => {
-   *    // ... expired
-   * })
-   * ```
-   */
-  expired: CustomEvent<void>;
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('changed', () => {
+//    *    // ... changed
+//    * })
+//    * ```
+//    */
+//   changed: CustomEvent<void>;
 
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('cancelled', () => {
-   *    // ... cancelled
-   * })
-   * ```
-   */
-  cancelled: CustomEvent<void>;
+//   /**
+//    * @example
+//    *
+//    * ```js
+//    * request.addEventListener('changed', () => {
+//    *    // ... changed
+//    * })
+//    * ```
+//    */
+//   offer: CustomEvent<Offer<CustomRequestQuery, CustomOfferOptions>>;
+// }
 
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('changed', () => {
-   *    // ... changed
-   * })
-   * ```
-   */
-  changed: CustomEvent<void>;
+// export class Request<
+//   CustomRequestQuery extends GenericQuery,
+//   CustomOfferOptions extends GenericOfferOptions,
+// > extends EventEmitter<RequestEvents<CustomRequestQuery, CustomOfferOptions>> {
+//   private querySchema: z.ZodType<CustomRequestQuery>;
+//   private offerOptionsSchema: z.ZodType<CustomOfferOptions>;
+//   private contractConfig: ContractConfig;
+//   private pubsub: CenterSub;
+//   private provider?: AbstractProvider;
+//   private subscriptionTimeout?: NodeJS.Timeout;
+//   data?: RequestData<CustomRequestQuery>; // Request message content
+//   topic?: string;
+//   offers: Set<Offer<CustomRequestQuery, CustomOfferOptions>>; // Offers
+//   published?: number; // Time in milliseconds
+//   received?: number; // Time in milliseconds
 
-  /**
-   * @example
-   *
-   * ```js
-   * request.addEventListener('changed', () => {
-   *    // ... changed
-   * })
-   * ```
-   */
-  offer: CustomEvent<Offer<CustomRequestQuery, CustomOfferOptions>>;
-}
+//   constructor(options: RequestInitOptions<CustomRequestQuery, CustomOfferOptions>) {
+//     super();
 
-export class Request<
-  CustomRequestQuery extends GenericQuery,
-  CustomOfferOptions extends GenericOfferOptions,
-> extends EventEmitter<RequestEvents<CustomRequestQuery, CustomOfferOptions>> {
-  private querySchema: z.ZodType<CustomRequestQuery>;
-  private offerOptionsSchema: z.ZodType<CustomOfferOptions>;
-  private contractConfig: ContractConfig;
-  private pubsub: CenterSub;
-  private provider?: AbstractProvider;
-  private subscriptionTimeout?: NodeJS.Timeout;
-  data?: RequestData<CustomRequestQuery>; // Request message content
-  topic?: string;
-  offers: Set<Offer<CustomRequestQuery, CustomOfferOptions>>; // Offers
-  published?: number; // Time in milliseconds
-  received?: number; // Time in milliseconds
+//     options = createRequestInitOptionsSchema<CustomRequestQuery, CustomOfferOptions>().parse(
+//       options,
+//     );
 
-  constructor(options: RequestInitOptions<CustomRequestQuery, CustomOfferOptions>) {
-    super();
+//     this.pubsub = options.pubsub;
+//     this.contractConfig = options.contractConfig;
+//     this.querySchema = options.querySchema;
+//     this.offerOptionsSchema = options.offerOptionsSchema;
+//     this.offers = new Set<Offer<CustomRequestQuery, CustomOfferOptions>>();
+//   }
 
-    options = createRequestInitOptionsSchema<CustomRequestQuery, CustomOfferOptions>().parse(
-      options,
-    );
+//   get subscribed(): boolean {
+//     return !!this.subscriptionTimeout;
+//   }
 
-    this.pubsub = options.pubsub;
-    this.contractConfig = options.contractConfig;
-    this.querySchema = options.querySchema;
-    this.offerOptionsSchema = options.offerOptionsSchema;
-    this.offers = new Set<Offer<CustomRequestQuery, CustomOfferOptions>>();
-  }
+//   toJSON() {
+//     if (!this.data) {
+//       throw new Error('Request not initialized yet');
+//     }
 
-  get subscribed(): boolean {
-    return !!this.subscriptionTimeout;
-  }
+//     return JSON.stringify(this.data);
+//   }
 
-  toJSON() {
-    if (!this.data) {
-      throw new Error('Request not initialized yet');
-    }
+//   toRaw(): RawRequest<CustomRequestQuery, CustomOfferOptions> {
+//     if (!this.data || !this.topic) {
+//       throw new Error('Request not initialized yet');
+//     }
 
-    return JSON.stringify(this.data);
-  }
+//     return {
+//       data: this.data,
+//       topic: this.topic,
+//       published: this.published,
+//       received: this.received,
+//       offers: Array.from(this.offers).map((o) => o.toRaw()),
+//     };
+//   }
 
-  toRaw(): RawRequest<CustomRequestQuery, CustomOfferOptions> {
-    if (!this.data || !this.topic) {
-      throw new Error('Request not initialized yet');
-    }
+//   toHash() {
+//     if (!this.data) {
+//       throw new Error('Request not initialized yet');
+//     }
 
-    return {
-      data: this.data,
-      topic: this.topic,
-      published: this.published,
-      received: this.received,
-      offers: Array.from(this.offers).map((o) => o.toRaw()),
-    };
-  }
+//     return hashObject(this.data, this.toJSON.bind(this));
+//   }
 
-  toHash() {
-    if (!this.data) {
-      throw new Error('Request not initialized yet');
-    }
+//   async build(topic: string, expire: string | number, nonce: number, query: CustomRequestQuery) {
+//     if (!topic) {
+//       throw new Error('Request topic is required');
+//     }
 
-    return hashObject(this.data, this.toJSON.bind(this));
-  }
+//     this.topic = topic;
+//     this.data = await buildRequest<CustomRequestQuery>(expire, nonce, query, this.querySchema);
+//     this.dispatchEvent(new CustomEvent<void>('built'));
+//     this.dispatchEvent(new CustomEvent<void>('changed'));
+//     logger.trace('Request data:', this.data);
+//   }
 
-  async build(topic: string, expire: string | number, nonce: number, query: CustomRequestQuery) {
-    if (!topic) {
-      throw new Error('Request topic is required');
-    }
+//   async buildRaw(rawRequest: RawRequest<CustomRequestQuery, CustomOfferOptions>) {
+//     this.topic = rawRequest.topic;
+//     this.offers = new Set<Offer<CustomRequestQuery, CustomOfferOptions>>(
+//       await Promise.all(
+//         rawRequest.offers.map(async (ro) => {
+//           const offer = new Offer({
+//             passive: true,
+//             pubsub: this.pubsub,
+//             provider: this.provider,
+//             contractConfig: this.contractConfig,
+//             querySchema: this.querySchema,
+//             offerOptionsSchema: this.offerOptionsSchema,
+//             supplierId: ro.data.payload.supplierId,
+//           });
+//           await offer.buildRaw(ro);
+//           return offer;
+//         }),
+//       ),
+//     );
+//     this.published = rawRequest.published;
+//     this.received = rawRequest.received;
 
-    this.topic = topic;
-    this.data = await buildRequest<CustomRequestQuery>(expire, nonce, query, this.querySchema);
-    this.dispatchEvent(new CustomEvent<void>('built'));
-    this.dispatchEvent(new CustomEvent<void>('changed'));
-    logger.trace('Request data:', this.data);
-  }
+//     this.data = await buildRequest<CustomRequestQuery>(
+//       rawRequest.data.expire,
+//       rawRequest.data.nonce,
+//       rawRequest.data.query as CustomRequestQuery, // @todo Fix it somehow
+//       this.querySchema,
+//       rawRequest.data.id,
+//     );
+//     this.dispatchEvent(new CustomEvent<void>('restored'));
+//     this.dispatchEvent(new CustomEvent<void>('changed'));
+//     logger.trace('Request data:', this.data);
 
-  async buildRaw(rawRequest: RawRequest<CustomRequestQuery, CustomOfferOptions>) {
-    this.topic = rawRequest.topic;
-    this.offers = new Set<Offer<CustomRequestQuery, CustomOfferOptions>>(
-      await Promise.all(
-        rawRequest.offers.map(async (ro) => {
-          const offer = new Offer({
-            passive: true,
-            pubsub: this.pubsub,
-            provider: this.provider,
-            contractConfig: this.contractConfig,
-            querySchema: this.querySchema,
-            offerOptionsSchema: this.offerOptionsSchema,
-            supplierId: ro.data.payload.supplierId,
-          });
-          await offer.buildRaw(ro);
-          return offer;
-        }),
-      ),
-    );
-    this.published = rawRequest.published;
-    this.received = rawRequest.received;
+//     if (this.published && rawRequest.topic && !isExpired(this.data.expire)) {
+//       this.publish().catch(logger.trace);
+//     }
+//   }
 
-    this.data = await buildRequest<CustomRequestQuery>(
-      rawRequest.data.expire,
-      rawRequest.data.nonce,
-      rawRequest.data.query as CustomRequestQuery, // @todo Fix it somehow
-      this.querySchema,
-      rawRequest.data.id,
-    );
-    this.dispatchEvent(new CustomEvent<void>('restored'));
-    this.dispatchEvent(new CustomEvent<void>('changed'));
-    logger.trace('Request data:', this.data);
+//   async publish(rePublish = false) {
+//     try {
+//       if (!this.data || !this.topic) {
+//         throw new Error('Request not initialized yet');
+//       }
 
-    if (this.published && rawRequest.topic && !isExpired(this.data.expire)) {
-      this.publish().catch(logger.trace);
-    }
-  }
+//       this.pubsub.subscribe(this.data.id);
+//       logger.trace(`Subscribed to request #${this.data.id}`);
+//       console.log('@@@', `Subscribed to request #${this.data.id}`);
 
-  async publish(rePublish = false) {
-    try {
-      if (!this.data || !this.topic) {
-        throw new Error('Request not initialized yet');
-      }
+//       if (!this.published || rePublish) {
+//         await this.pubsub.publish(this.topic, encodeText(this.toJSON()));
+//       }
 
-      this.pubsub.subscribe(this.data.id);
+//       this.subscriptionTimeout = setTimeout(() => {
+//         this.dispatchEvent(new CustomEvent<void>('expired'));
+//         this.cancel();
+//       }, (this.data.expire - nowSec()) * 1000);
 
-      if (!this.published || rePublish) {
-        await this.pubsub.publish(this.topic, encodeText(this.toJSON()));
-      }
+//       this.published = nowSec();
+//       this.dispatchEvent(new CustomEvent<void>('published'));
+//       this.dispatchEvent(new CustomEvent<void>('changed'));
+//       logger.trace(`Request #${this.data.id} published at`, this.published);
+//     } catch (error) {
+//       logger.error(error);
+//       this.cancel();
+//       throw error;
+//     }
+//   }
 
-      this.subscriptionTimeout = setTimeout(() => {
-        this.dispatchEvent(new CustomEvent<void>('expired'));
-        this.cancel();
-      }, (this.data.expire - nowSec()) * 1000);
+//   cancel() {
+//     try {
+//       if (!this.data) {
+//         throw new Error('Request not initialized yet');
+//       }
 
-      this.published = nowSec();
-      this.dispatchEvent(new CustomEvent<void>('published'));
-      this.dispatchEvent(new CustomEvent<void>('changed'));
-      logger.trace(`Request #${this.data.id} published at`, this.published);
-    } catch (error) {
-      logger.error(error);
-      this.cancel();
-      throw error;
-    }
-  }
+//       this.pubsub.unsubscribe(this.data.id);
+//       clearTimeout(this.subscriptionTimeout);
+//       this.subscriptionTimeout = undefined;
+//       this.dispatchEvent(new CustomEvent<void>('cancelled'));
+//       this.dispatchEvent(new CustomEvent<void>('changed'));
+//       logger.trace('Unsubscribed from:', this.data.id);
+//     } catch (error) {
+//       logger.error(error);
+//       throw error;
+//     }
+//   }
 
-  cancel() {
-    try {
-      if (!this.data) {
-        throw new Error('Request not initialized yet');
-      }
+//   addOffer(offer: Offer<CustomRequestQuery, CustomOfferOptions>) {
+//     try {
+//       if (!this.data) {
+//         throw new Error('Request not initialized yet');
+//       }
 
-      this.pubsub.unsubscribe(this.data.id);
-      clearTimeout(this.subscriptionTimeout);
-      this.subscriptionTimeout = undefined;
-      this.dispatchEvent(new CustomEvent<void>('cancelled'));
-      this.dispatchEvent(new CustomEvent<void>('changed'));
-      logger.trace('Unsubscribed from:', this.data.id);
-    } catch (error) {
-      logger.error(error);
-      throw error;
-    }
-  }
+//       if (!offer.verifyRequestId(this.data.id)) {
+//         throw new Error('Invalid request');
+//       }
 
-  addOffer(offer: Offer<CustomRequestQuery, CustomOfferOptions>) {
-    try {
-      if (!this.data) {
-        throw new Error('Request not initialized yet');
-      }
-
-      if (!offer.verifyRequestId(this.data.id)) {
-        throw new Error('Invalid request');
-      }
-
-      this.offers.add(offer);
-      this.dispatchEvent(
-        new CustomEvent<Offer<CustomRequestQuery, CustomOfferOptions>>('offer', {
-          detail: offer,
-        }),
-      );
-    } catch (error) {
-      logger.error(error);
-    }
-  }
-}
+//       this.offers.add(offer);
+//       this.dispatchEvent(
+//         new CustomEvent<Offer<CustomRequestQuery, CustomOfferOptions>>('offer', {
+//           detail: offer,
+//         }),
+//       );
+//     } catch (error) {
+//       logger.error(error);
+//     }
+//   }
+// }
