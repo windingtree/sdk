4b3ff62e8fa421d5c3f31352210f5dd2f73735d7
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-03-30 11:08:55 +0200

    fix: 🐛 Removed outdated links in the index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,5 @@ export * from './server/index.js';
 export * from './client/index.js';
 export * from './node/index.js';
 export * from './shared/messages.js';
-export * from './shared/request.js';
-export * from './shared/offer.js';
 export * from './shared/options.js';
 export * from './shared/queue.js';
