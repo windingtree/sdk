d1fac83f0e492590dbe02a2d18d15b228e8faa8e
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-06-01 21:48:45 +0200

    refactor: 💡 Client example refactored into multiple components

--- examples/client/src/App.tsx
+++ examples/client/src/App.tsx
@@ -1,19 +1,20 @@
-import { useState, useEffect, useRef, useCallback } from 'react';
-import { Hash, stringify } from 'viem';
+import { useState, useEffect, useRef } from 'react';
 import {
   Client,
   ClientOptions,
-  RequestRecord,
   createClient,
   storage,
-  utils,
 } from '../../../src/index.js'; // @windingtree/sdk
 import { RequestQuery, OfferOptions, chainConfig, serverAddress } from '../../shared/index.js';
 import { OfferData } from '../../../src/shared/types.js';
-import { centerEllipsis, formatBalance, parseWalletError } from './utils.js';
 import { useWallet } from './providers/WalletProvider/WalletProviderContext.js';
 import { ConnectButton } from './providers/WalletProvider/ConnectButton.js';
-import { ZeroHash } from './utils.js';
+import { FormValues, RequestForm } from './components/RequestForm.js';
+import { Tabs, TabPanel } from './components/Tabs.js';
+import { Requests, RequestsRegistryRecord } from './components/Requests.js';
+import { MakeDeal } from './components/MakeDeal.js';
+import { Offers } from './components/Offers.js';
+import { Deals, DealsRegistryRecord } from './components/Deals.js';
 
 /** Default request expiration time */
 const defaultExpire = '30s';
@@ -21,316 +22,16 @@ const defaultExpire = '30s';
 /** Default topic to publish requests the same as for the supplier node */
 const defaultTopic = 'hello';
 
-type RequestsRegistryRecord = Required<RequestRecord<RequestQuery, OfferOptions>>;
-
-interface FormValues {
-  topic: string;
-  message: string;
-}
-
-interface RequestFormProps {
-  connected: boolean;
-  onSubmit(query: FormValues): void;
-}
-
-interface RequestsProps {
-  requests: RequestsRegistryRecord[];
-  subscribed?: (id: string) => boolean;
-  onClear(): void;
-  onCancel(id: string): void;
-  onOffers(offers: OfferData<RequestQuery, OfferOptions>[]): void;
-}
-
-interface OffersProps {
-  offers?: OfferData<RequestQuery, OfferOptions>[];
-  onAccept(offers: OfferData<RequestQuery, OfferOptions>): void;
-  onClose: () => void;
-}
-
-interface MakeDealProps {
-  offer?: OfferData<RequestQuery, OfferOptions>;
-  client?: Client<RequestQuery, OfferOptions>;
-}
-
-/**
- * Accepts user input
- */
-export const RequestForm = ({ connected, onSubmit }: RequestFormProps) => {
-  const [topic, setTopic] = useState<string>(defaultTopic);
-  const [message, setMessage] = useState<string>('');
-
-  if (!connected) {
-    return null;
-  }
-
-  return (
-    <div style={{ marginTop: 20 }}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (message === '') {
-            return;
-          }
-          onSubmit({
-            topic,
-            message,
-          });
-        }}
-      >
-        <div style={{ marginBottom: 20 }}>
-          <div>
-            <strong>Topic:</strong>
-          </div>
-          <div style={{ marginBottom: 5 }}>
-            <input value={topic} onChange={(e) => setTopic(e.target.value)} />
-          </div>
-          <div>
-            <strong>Request:</strong>
-          </div>
-          <div>
-            <input value={message} onChange={(e) => setMessage(e.target.value)} />
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center', marginTop: 10 }}>
-            <div>
-              <button type="submit">Send</button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-};
-
-/**
- * Published requests table
- */
-export const Requests = ({ requests, subscribed, onClear, onCancel, onOffers }: RequestsProps) => {
-  if (requests.length === 0) {
-    return null;
-  }
-
-  return (
-    <div style={{ marginTop: 20 }}>
-      <h3>Requests</h3>
-      <table border={1} cellPadding={5}>
-        <thead>
-          <tr>
-            <td>Topic</td>
-            <td>Id</td>
-            <td>Query</td>
-            <td>Subscribed</td>
-            <td>Expired</td>
-            <td>Offers</td>
-            <td>Cancel</td>
-          </tr>
-        </thead>
-        <tbody>
-          {requests.map((r, index) => (
-            <tr key={index}>
-              <td>{r.data.topic}</td>
-              <td>{centerEllipsis(r.data.id)}</td>
-              <td>{JSON.stringify(r.data.query)}</td>
-              <td>{subscribed && subscribed(r.data.id) ? '✅' : 'no'}</td>
-              <td>{utils.isExpired(r.data.expire) || r.cancelled ? '✅' : 'no'}</td>
-              <td>
-                {r.offers.length === 0 ? 0 : ''}
-                {r.offers.length > 0 && (
-                  <button onClick={() => onOffers(r.offers)}>{r.offers.length}</button>
-                )}
-              </td>
-              <td>
-                {!r.cancelled && !utils.isExpired(r.data.expire) ? (
-                  <button
-                    onClick={() => {
-                      onCancel(r.data.id);
-                    }}
-                  >
-                    Cancel
-                  </button>
-                ) : (
-                  'cancelled'
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div style={{ marginTop: 10 }}>
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            onClear();
-          }}
-        >
-          Clear
-        </button>
-      </div>
-    </div>
-  );
-};
-
-/**
- * Received offers table
- */
-export const Offers = ({ offers, onAccept, onClose }: OffersProps) => {
-  const [offerStates, setOfferStates] = useState<Record<string, boolean>>({});
-
-  useEffect(() => {
-    if (offers && offers.length > 0) {
-      const expireHandler = () => {
-        const newOfferStates: Record<string, boolean> = {};
-        offers.forEach((offer) => {
-          newOfferStates[offer.id] = utils.isExpired(offer.expire);
-        });
-        setOfferStates(newOfferStates);
-      };
-
-      const interval = setInterval(expireHandler, 1000);
-      expireHandler();
-
-      return () => clearInterval(interval);
-    }
-  }, [offers]);
-
-  if (!offers || offers.length === 0) {
-    return null;
-  }
-
-  return (
-    <div style={{ marginTop: 20 }}>
-      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-        <div style={{ flex: 1 }}><h3>Offers</h3></div>
-        <div style={{ flex: 1 }}><button onClick={onClose}>Close</button></div>
-      </div>
-      <table border={1} cellPadding={5}>
-        <thead>
-          <tr>
-            <td>Id</td>
-            <td>Data</td>
-            <td>Expired</td>
-            <td>Action</td>
-          </tr>
-        </thead>
-        <tbody>
-          {offers.map((o, index) => (
-            <tr key={index}>
-              <td>{centerEllipsis(o.id)}</td>
-              <td>{stringify(o.options)}</td>
-              <td>{offerStates[o.id] ? '✅' : 'no'}</td>
-              <td>
-                {!offerStates[o.id] ? <button onClick={() => onAccept(o)}>Accept</button> : ''}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-/**
- * Making of deal form
- */
-export const MakeDeal = ({ offer, client }: MakeDealProps) => {
-  const { account, publicClient, walletClient } = useWallet();
-  const [tx, setTx] = useState<string | undefined>();
-  const [error, setError] = useState<string | undefined>();
-  const [loading, setLoading] = useState<boolean>(false);
-
-  const dealHandler = useCallback(
-    async (paymentId: Hash) => {
-      try {
-        setTx(undefined);
-        setError(undefined);
-        setLoading(true);
-
-        if (!client) {
-          throw new Error('Client not ready');
-        }
-
-        if (!publicClient || !walletClient) {
-          throw new Error('Ethereum client not ready');
-        }
-
-        if (!offer) {
-          throw new Error('Invalid deal configuration');
-        }
-
-        await client.deals.create(offer, paymentId, ZeroHash, publicClient, walletClient, setTx);
-        setLoading(false);
-      } catch (err) {
-        console.log(err);
-        setError(parseWalletError(err));
-        setLoading(false);
-      }
-    },
-    [client, offer, publicClient, walletClient],
-  );
-
-  if (!offer) {
-    return null;
-  }
-
-  if (!account) {
-    return (
-      <div style={{ marginTop: 20 }}>
-        <h3>Make a deal on offer: {centerEllipsis(offer.id)}</h3>
-        <div>Please connect your wallet to continue</div>
-        <div>
-          <ConnectButton />
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div style={{ marginTop: 20 }}>
-      <h3>Make a deal on offer: {centerEllipsis(offer.id)}</h3>
-      <div>Offer options: {stringify(offer.options)}</div>
-      <div>To make a deal choose a price option:</div>
-      <div>
-        <table border={1} cellPadding={5}>
-          <thead>
-            <tr>
-              <td>Id</td>
-              <td>Asset</td>
-              <td>Price</td>
-              <td>Action</td>
-            </tr>
-          </thead>
-          <tbody>
-            {offer.payment.map((p, index) => (
-              <tr key={index}>
-                <td>{centerEllipsis(p.id)}</td>
-                <td>{p.asset}</td>
-                <td>{formatBalance(p.price, 4)}</td>
-                <td>
-                  <button onClick={() => dealHandler(p.id)}>Deal</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {tx && <div style={{ marginTop: 20 }}>Tx hash: {tx}</div>}
-      {loading && <div style={{ marginTop: 20 }}>Loading...</div>}
-      {error && (
-        <div style={{ marginTop: 20, padding: 10, backgroundColor: 'rgba(0,0,0,0.1)' }}>
-          {error}
-        </div>
-      )}
-    </div>
-  );
-};
-
 /**
  * Main application component
  */
 export const App = () => {
   const client = useRef<Client<RequestQuery, OfferOptions> | undefined>();
-  const { account, balance } = useWallet();
+  const { account, balance, publicClient } = useWallet();
   const [connected, setConnected] = useState<boolean>(false);
+  const [selectedTab, setSelectedTab] = useState<number>(0);
   const [requests, setRequests] = useState<RequestsRegistryRecord[]>([]);
+  const [deals, setDeals] = useState<DealsRegistryRecord[]>([]);
   const [offers, setOffers] = useState<OfferData<RequestQuery, OfferOptions>[] | undefined>();
   const [offer, setOffer] = useState<OfferData<RequestQuery, OfferOptions> | undefined>();
   const [error, setError] = useState<string | undefined>();
@@ -345,18 +46,34 @@ export const App = () => {
           chain: chainConfig,
           serverAddress,
           storageInitializer: storage.localStorage.createInitializer({
-            session: true,
+            session: true, // session or local storage
           }),
           dbKeysPrefix: 'wt_',
+          publicClient,
+        };
+
+        const updateRequests = () => {
+          if (!client.current) {
+            return;
+          }
+          setRequests(client.current.requests.getAll());
+        };
+
+        const updateDeals = () => {
+          if (!client.current) {
+            return;
+          }
+          client.current.deals.getAll()
+            .then(setDeals)
+            .catch(console.error);
         };
 
         client.current = createClient<RequestQuery, OfferOptions>(options);
 
         client.current.addEventListener('start', () => {
           console.log('🚀 Client started at:', new Date().toISOString());
-          if (client.current) {
-            setRequests(client.current.requests.getAll());
-          }
+          updateRequests();
+          updateDeals();
         });
 
         client.current.addEventListener('stop', () => {
@@ -373,14 +90,7 @@ export const App = () => {
           console.log('🔌 Client disconnected from server at:', new Date().toISOString());
         });
 
-        const updateRequests = () => {
-          if (!client.current) {
-            return;
-          }
-          setRequests(client.current.requests.getAll());
-        };
-
-        /** Listening for requests events and update the table */
+        /** Listening for requests events and update tables */
         client.current.addEventListener('request:create', updateRequests);
         client.current.addEventListener('request:subscribe', updateRequests);
         client.current.addEventListener('request:publish', updateRequests);
@@ -390,6 +100,7 @@ export const App = () => {
         client.current.addEventListener('request:delete', updateRequests);
         client.current.addEventListener('request:offer', updateRequests);
         client.current.addEventListener('request:clear', updateRequests);
+        client.current.addEventListener('deal:changed', updateDeals);
 
         await client.current.start();
       } catch (error) {
@@ -444,24 +155,44 @@ export const App = () => {
       </div>
       {client.current && <div>✅ Client started</div>}
       {connected && <div>✅ Connected to the coordination server</div>}
-      <RequestForm connected={connected} onSubmit={sendRequest} />
-      <Requests
-        requests={requests}
-        subscribed={(id) => client.current?.requests.subscribed(id) || false}
-        onClear={() => {
-          if (client.current) {
-            client.current?.requests.clear();
-          }
-        }}
-        onCancel={(id) => {
-          if (client.current) {
-            client.current.requests.cancel(id);
-          }
-        }}
-        onOffers={setOffers}
-      />
-      <Offers offers={offers} onAccept={setOffer} onClose={() => setOffers(undefined)} />
-      <MakeDeal offer={offer} client={client.current} />
+      <RequestForm connected={connected} onSubmit={sendRequest} defaultTopic={defaultTopic} />
+      <Tabs tabs={[
+        {
+          id: 0,
+          title: 'Requests',
+          active: true
+        },
+        {
+          id: 1,
+          title: 'Deals'
+        },
+      ]} onChange={setSelectedTab} />
+      <TabPanel id={0} activeTab={selectedTab}>
+        <Requests
+          requests={requests}
+          subscribed={(id) => client.current?.requests.subscribed(id) || false}
+          onClear={() => {
+            if (client.current) {
+              client.current?.requests.clear();
+            }
+          }}
+          onCancel={(id) => {
+            if (client.current) {
+              client.current.requests.cancel(id);
+            }
+          }}
+          onOffers={setOffers}
+        />
+        <Offers offers={offers} onAccept={setOffer} onClose={() => {
+          setOffer(undefined);
+          setOffers(undefined);
+        }} />
+        <MakeDeal offer={offer} client={client.current} />
+      </TabPanel>
+      <TabPanel id={1} activeTab={selectedTab}>
+        <Deals deals={deals} client={client.current} />
+      </TabPanel>
+
       {error && <div style={{ marginTop: 20 }}>🚨 {error}</div>}
     </>
   );
