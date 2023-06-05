9729bddf695e625c477f34025ab2d178e97ee636
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-06-06 00:46:54 +0200

    refactor: 💡 DealStatus enum moved to constants

--- src/constants.ts
+++ src/constants.ts
@@ -15,3 +15,17 @@ export const queueJobAttemptsDelay = 1000;
 
 /** Time interval for queue needs in milliseconds */
 export const queueHeartbeat = 5;
+
+/**
+ * Deal status
+ */
+export enum DealStatus {
+  Created, // Just created
+  Claimed, // Claimed by the supplier
+  Rejected, // Rejected by the supplier
+  Refunded, // Refunded by the supplier
+  Cancelled, // Cancelled by the buyer
+  CheckedIn, // Checked In
+  CheckedOut, // Checked Out
+  Disputed, // Dispute started
+}
