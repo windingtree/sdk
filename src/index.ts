b99061e9c24972279b4a17fd11deffe19cfbfbe1
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-06-08 14:41:59 +0200

    refactor: 💡 Constants now are exported in common interface

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export * as constants from './constants.js';
 export * as storage from './storage/index.js';
 export * as utils from './utils/index.js';
+export * from './constants.js';
 export * from './server/index.js';
 export * from './client/index.js';
 export * from './node/index.js';
