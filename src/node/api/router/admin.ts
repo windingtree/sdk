05669895fa8d6101e2d207de6a99e5120de36063
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-06-30 21:08:46 +0200

    refactor: 💡 Using of middleware with type overrides

--- src/node/api/router/admin.ts
+++ src/node/api/router/admin.ts
@@ -3,7 +3,7 @@ import { Address, Hash, verifyTypedData } from 'viem';
 import { TypedDataDomain } from 'abitype';
 import { Account } from '../../../index.js';
 import { User, UserInputSchema } from '../../db/users.js';
-import { router, procedure } from '../index.js';
+import { router, procedure, withOwnerAccount } from '../index.js';
 import { createLogger } from '../../../utils/logger.js';
 
 const logger = createLogger('AdminRouter');
@@ -73,20 +73,18 @@ export const adminRouter = router({
    * Throws an error if the user already exists or signature provided is invalid.
    */
   register: procedure
+    .use(withOwnerAccount)
     .input(UserInputSchema)
     .mutation(async ({ input, ctx }) => {
       const { login, password } = input;
-      const { server } = ctx;
+      const { ownerAccount, users } = ctx;
 
       try {
-        if (
-          !server.ownerAccount ||
-          !(await verifyAdminSignature(server.ownerAccount, password as Hash))
-        ) {
+        if (!(await verifyAdminSignature(ownerAccount, password as Hash))) {
           throw new Error('Invalid signature');
         }
 
-        await server.users.add(login, password, true);
+        await users.add(login, password, true);
         logger.trace(`Admin registered`);
       } catch (error) {
         logger.error('admin.register', error);
@@ -101,32 +99,32 @@ export const adminRouter = router({
    * Log in an existing admin.
    * If successful, generates a new access token and sends it in the response.
    */
-  login: procedure.input(UserInputSchema).mutation(async ({ input, ctx }) => {
-    const { login, password } = input;
-    const { server, updateAccessToken } = ctx;
-    let user: User;
+  login: procedure
+    .use(withOwnerAccount)
+    .input(UserInputSchema)
+    .mutation(async ({ input, ctx }) => {
+      const { login, password } = input;
+      const { ownerAccount, users, updateAccessToken } = ctx;
+      let user: User;
 
-    try {
-      logger.trace(`Trying to log in admin ${login}`);
-      user = await server.users.get(login);
-    } catch (error) {
-      logger.error('admin.login', error);
-      throw new TRPCError({
-        code: 'BAD_REQUEST',
-        message: (error as Error).message,
-      });
-    }
+      try {
+        logger.trace(`Trying to log in admin ${login}`);
+        user = await users.get(login);
+      } catch (error) {
+        logger.error('admin.login', error);
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: (error as Error).message,
+        });
+      }
 
-    if (
-      !server.ownerAccount ||
-      !(await verifyAdminSignature(server.ownerAccount, password as Hash))
-    ) {
-      throw new TRPCError({
-        code: 'UNAUTHORIZED',
-        message: 'Invalid signature',
-      });
-    }
+      if (!(await verifyAdminSignature(ownerAccount, password as Hash))) {
+        throw new TRPCError({
+          code: 'UNAUTHORIZED',
+          message: 'Invalid signature',
+        });
+      }
 
-    await updateAccessToken(user);
-  }),
+      await updateAccessToken(user);
+    }),
 });
