c8a2b75fb6e492298c4c20956afd3342af3f895a
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-07-04 23:20:48 +0200

    refactor: 💡 Updated storage initializer typing

--- src/storage/abstract.ts
+++ src/storage/abstract.ts
@@ -27,6 +27,7 @@ export type StorageInitializer<T extends Storage = Storage> = () => Promise<T>;
 /**
  * Storage initializer callback function type
  */
-export type StorageInitializerFunction<T extends Storage = Storage> = (
-  options?: GenericStorageOptions,
-) => StorageInitializer<T>;
+export type StorageInitializerFunction<
+  T extends Storage = Storage,
+  O extends GenericStorageOptions = GenericStorageOptions,
+> = (options?: O) => StorageInitializer<T>;
